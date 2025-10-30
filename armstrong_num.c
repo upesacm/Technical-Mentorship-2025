90fccb2b3164910e83aec6dbb99309d74a847999
sarveshwari08 <sarveshwarii8686@gmail.com>
2025-10-30 20:22:58 +0530

    Add Armstrong number checker in armstrong_num.c

---
+++ armstrong_num.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    int num,digit,armstrongn;
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+    armstrongn = 0;
+    int originalNum = num;
+    while(originalNum != 0){
+    int remainder = originalNum % 10;
+    armstrongn += remainder * remainder * remainder;
+    originalNum /= 10;
+    digit++;
+    } printf("The Armstrong number is: %d\n", armstrongn);
+    if(armstrongn == num)
+        printf("%d is an Armstrong number.", num);
+    else
+        printf("%d is not an Armstrong number.", num);
+}
